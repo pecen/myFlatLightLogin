c6e5aa256bd5e7415c5d0cdff24451584517f13d
RedesajnP50PeCen\Peter Centellini <peter.centellini@redesajn.com>
2023-03-10 22:44:53 +0100

    Added project files and corrected the path for the background image

---
+++ src/myFlatLightLogin.UI.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FlatLightLogin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private bool IsMaximize = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximize)
+                {
+                    WindowState = WindowState.Normal;
+                    Width = 1024;
+                    Height = 720;
+
+                    IsMaximize = false;
+                }
+                else
+                {
+                    WindowState = WindowState.Maximized;
+                    IsMaximize = true;
+                }
+            }
+        }
+    }
+}
