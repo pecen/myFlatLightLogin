1453cfe7f7b54d39779d1fe3539d180447c178a9
RedesajnP50PeCen\Peter Centellini <peter.centellini@redesajn.com>
2023-03-11 01:33:06 +0100

    Removed code-behind in favour of Mvvm commands

--- src/myFlatLightLogin.UI.Wpf/MVVM/View/MainWindow.xaml.cs
+++ src/myFlatLightLogin.UI.Wpf/MVVM/View/MainWindow.xaml.cs
@@ -13,34 +13,5 @@ namespace FlatLightLogin
         {
             InitializeComponent();
         }
-
-        //private void Border_MouseDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    if (e.ChangedButton == MouseButton.Left)
-        //    {
-        //        DragMove();
-        //    }
-        //}
-
-        private bool IsMaximize = false;
-        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2)
-            {
-                if (IsMaximize)
-                {
-                    WindowState = WindowState.Normal;
-                    Width = 1024;
-                    Height = 720;
-
-                    IsMaximize = false;
-                }
-                else
-                {
-                    WindowState = WindowState.Maximized;
-                    IsMaximize = true;
-                }
-            }
-        }
     }
 }
