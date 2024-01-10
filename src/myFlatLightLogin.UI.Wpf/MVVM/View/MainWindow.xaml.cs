54dc055b3c5aa68b4c671cdb44a60ce58b6591df
NEONODE\peter.centellini <peter.centellini@neonode.com>
2024-01-10 10:39:52 +0100

    Added Metro look (MahApp). Added extra functionality  with a shutdown popup (MahApp).

--- src/myFlatLightLogin.UI.Wpf/MVVM/View/MainWindow.xaml.cs
+++ src/myFlatLightLogin.UI.Wpf/MVVM/View/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,11 +10,52 @@ namespace myFlatLightLogin.UI.Wpf.MVVM.View
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        private bool _shutdown;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (e.Cancel)
+            {
+                return;
+            }
+
+            if (_shutdown == false)
+            {
+                e.Cancel = true;
+
+                // We have to delay the execution through BeginInvoke to prevent potential re-entrancy
+                Dispatcher.BeginInvoke(new Action(async () => await ConfirmShutdown()));
+            }
+        }
+
+        private async Task ConfirmShutdown()
+        {
+            var mySettings = new MetroDialogSettings
+            {
+                AffirmativeButtonText = "Quit",
+                NegativeButtonText = "Cancel",
+                AnimateShow = true,
+                AnimateHide = false
+            };
+
+            var result = await this.ShowMessageAsync("Quit application?",
+                                                     "Sure you want to quit application?",
+                                                     MessageDialogStyle.AffirmativeAndNegative,
+                                                     mySettings);
+
+            _shutdown = result == MessageDialogResult.Affirmative;
+
+            if (_shutdown)
+            {
+                Application.Current.Shutdown();
+            }
         }
     }
 }
